337b660eda25cb805096f9e2efe5c1fece1a4036
sheenamsehmi <115872687+sheenamsehmi@users.noreply.github.com>
2022-10-22 20:23:25 +0100

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,25 @@
+# def findLen(str):
+#   counter = 0   
+
+#  for i in str:        counter += 1    return counter  str = "geeks"print(findLen(str))`
+
+
+# with geeks 
+# geeks = (1,2,3,4,5)
+
+# str == "geeks"
+# geeks ==(1,2,3,4,5)
+# print(len(str))
+
+# str = "geeks"
+# print(len(str))
+
+
+# def findLen(str):
+#     counter = 0    
+#     for i in str:
+#         counter += 1
+#     return counter
+
+# str = "geeks"
+# print(len(str))
